de4a1d7de6467afeed731be87ac78cc12917f73a
shreeyansh64 <shreeyansh64@gmail.com>
2025-03-26 19:12:03 +0530

    Taking in Inputs

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const bp = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = exp();
 
+const user = [{
+    "house" : [],
+    "bank" : []
+}]
+
 app.use(exp.json());
+app.use(bp.json())
+
+app.get('/market',function(req,res){
+    const house = req.headers.house;
+    console.log(house)
+    const bank = req.headers.bank;
+    user[0].house.push(house);
+    user[0].bank.push(bank);
+    console.log(user[0])
+
+    res.send("Hello")
+})
+
 
 app.get('/',(req,res)=>{
     console.log(req.headers)
